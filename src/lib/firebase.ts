63b2510faa7415e7708c105104649bc332a95e99
reza rajabalizadeh <reza6.rajabalizadeh@gmail.com>
2025-08-28 15:33:06 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "dosewise-2con1",
+  "appId": "1:85196976817:web:1d7bc59b9153af5c79a890",
+  "storageBucket": "dosewise-2con1.firebasestorage.app",
+  "apiKey": "AIzaSyCswZ5-mKAEonjOZzJnPFeI766hRRcIShM",
+  "authDomain": "dosewise-2con1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "85196976817"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
